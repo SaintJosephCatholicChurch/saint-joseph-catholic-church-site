d9996ec018e06ceece6eddf5dc64db0415fcb13a
Daniel Lautzenheiser <lautzd@gmail.com>
2022-08-31 09:04:09 -0400

    Use PWA only in prod and analyzer only in dev

--- next.config.js
+++ next.config.js
@@ -6,23 +6,29 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    pageExtensions: ['tsx'],
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.module.rules.push(
-        ...[
-          {
-            test: /\.yml$/,
-            use: 'yaml-loader'
-          },
-          {
-            test: /\.svg$/,
-            use: '@svgr/webpack'
-          }
-        ]
-      );
-      return config;
-    }
-  })
-);
+let config = {
+  pageExtensions: ['tsx'],
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.yml$/,
+          use: 'yaml-loader'
+        },
+        {
+          test: /\.svg$/,
+          use: '@svgr/webpack'
+        }
+      ]
+    );
+    return config;
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config = withPWA(config);
+} else {
+  config = withBundleAnalyzer(config);
+}
+
+module.exports = config;

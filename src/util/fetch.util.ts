6c1479d7a3297031481d79728712ee4ad66bc6e0
Daniel Lautzenheiser <lautzd@gmail.com>
2022-09-09 10:01:15 -0400

    Fix images, add parish membership page

--- src/util/fetch.util.ts
+++ src/util/fetch.util.ts
@@ -1,5 +1,10 @@
 export async function doesUrlFileExist(url: string): Promise<{ type: string; exists: boolean }> {
   const cleanUrl = url.replace(/^blob:/g, '');
+  if (!cleanUrl.startsWith(window.location.hostname)) {
+    return { type: 'Unknown', exists: true };
+  }
+
+  console.log('clean url', cleanUrl);
   const response = await fetch(cleanUrl, { method: 'HEAD' });
   return { type: response.headers.get('Content-Type'), exists: response.ok };
 }

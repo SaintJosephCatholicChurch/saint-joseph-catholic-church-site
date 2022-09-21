1a57ed2d83f946b85091a97a2304c226aad40940
Daniel Lautzenheiser <lautzd@gmail.com>
2022-09-21 10:10:59 -0400

    Disable react devtools in prod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,10 @@ import { AppProps } from 'next/app';
 import 'normalize.css';
 import '../../public/styles/global.css';
 
+if (process.env.NODE_ENV == 'production') {
+  (window as any).__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function () {};
+}
+
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };

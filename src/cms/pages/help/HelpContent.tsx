d7c2b0591376ad7e8ccb8e189b43b225041207a6
Daniel Lautzenheiser <lautzd@gmail.com>
2022-10-28 14:25:47 -0400

    Add rest of help page content

--- src/cms/pages/help/HelpContent.tsx
+++ src/cms/pages/help/HelpContent.tsx
@@ -12,7 +12,8 @@ const StyledContent = styled('main')`
   flex-direction: column;
   gap: 8px;
 
-  p + strong {
+  p + strong,
+  ul + strong {
     margin-top: 16px;
   }
 
@@ -45,13 +46,13 @@ const StyledContent = styled('main')`
   h2 {
     font-size: 36px;
     font-weight: 700;
-    margin: 16px 0 0;
+    margin: 24px 0 0;
   }
 
   h3 {
     font-size: 24px;
     font-weight: 500;
-    margin: 8px 0 0;
+    margin: 16px 0 0;
   }
 
   h4,
@@ -83,14 +84,14 @@ const HelpContent = () => {
       <HelpHeading variant="h2">FAQ</HelpHeading>
       <strong>How long will it take for my changes to appear on the live site?</strong>
       <p>Changes made in the admin panel will appear on the live site within 4 to 10 minutes.</p>
-      <strong>Oops I delete something I didn&apos;t want to. What do I do now?</strong>
+      <strong>Oops I deleted something I didn&apos;t want to. What do I do now?</strong>
       <p>
         The site is stored on GitHub which has built in version control (history). While you cannot bring back changes
         from the admin panel, rest assured all of the changes are saved. Reach out to your site admin for help restoring
         the lost data.
       </p>
       <HelpHeading variant="h2">Church Details</HelpHeading>
-      <p>The church details covers general details about the chruch.</p>
+      <p>This section controls general details about the chruch.</p>
       <HelpHeading variant="h3">General Church Details</HelpHeading>
       <p>This section includes:</p>
       <ul>
@@ -262,16 +263,18 @@ const HelpContent = () => {
           <strong>Image</strong> - Optional. An image to show at the top of the post.
         </li>
         <li>
-          <strong>Publish Date</strong> - The date the news article was published. This is used to sort the news with the newest articles appearing at the top (and in the Recent news widget).
+          <strong>Publish Date</strong> - The date the news article was published. This is used to sort the news with
+          the newest articles appearing at the top (and in the Recent news widget).
         </li>
         <li>
-          <strong>Tags</strong> - Optional. The tags for the post. Used to group similar posts. See <HelpHeadingLink>Tags</HelpHeadingLink>
+          <strong>Tags</strong> - Optional. The tags for the post. Used to group similar posts. See{' '}
+          <HelpHeadingLink>Tags</HelpHeadingLink>
         </li>
         <li>
           <strong>Body</strong> - The content of the post.
         </li>
       </ul>
-      <HelpHeading variant="h2">Bulletins</HelpHeading>
+      <HelpHeading variant="h2">Pages</HelpHeading>
       <p>This section controls all general pages not covered by another section of the admin page.</p>
       <p>This section includes:</p>
       <ul>
@@ -289,7 +292,124 @@ const HelpContent = () => {
         </li>
       </ul>
       <HelpHeading variant="h2">Tags</HelpHeading>
-      <p>This section controls all of the tags for grouping simliar posts. You must create a tag here for it to appear in <HelpHeadingLink>News Posts</HelpHeadingLink>.</p>
+      <p>
+        This section controls all of the tags for grouping simliar posts. You must create a tag here for it to appear in{' '}
+        <HelpHeadingLink>News Posts</HelpHeadingLink>.
+      </p>
+      <p>This section includes:</p>
+      <ul>
+        <li>
+          <strong>Tag</strong> - The name of the tag. It appears directly under the image (or title if no image) of a
+          news post with a <code>#</code> symbol in front. Tags can be clicked to show all posts with that tag.
+        </li>
+      </ul>
+      <HelpHeading variant="h2">Site Config</HelpHeading>
+      <p>This section controls all site configuration related items including the navigation menus.</p>
+      <HelpHeading variant="h3">General Site Config</HelpHeading>
+      <p>
+        This section controls all of the meta and{' '}
+        <a href="https://searchengineland.com/guide/what-is-seo">SEO (Search Engine Optimzation)</a>.
+      </p>
+      <p>This section includes:</p>
+      <ul>
+        <li>
+          <strong>URL</strong> - The base url for the website.
+        </li>
+        <li>
+          <strong>Site Title</strong> - The site&apos;s title. This appears in the tab header / title bar for the
+          webpage. Post titles are appended in front of it when on a post page.
+        </li>
+        <li>
+          <strong>Site Description</strong> - A description of the site. This is displayed anywhere the site is linked
+          (especially in social media apps).
+        </li>
+        <li>
+          <strong>Site Image</strong> - An image for the site. This is displayed anywhere the site is linked (especially
+          in social media apps).
+        </li>
+        <li>
+          <strong>Site Keywords</strong> - Keywords for search engines (google, bing, etc) to pick up on. (See{' '}
+          <a href="https://searchengineland.com/guide/what-is-seo">SEO (Search Engine Optimzation)</a>)
+        </li>
+        <li>
+          <strong>News Posts Per Page</strong> - How many news posts should appear on each page in the news section.
+        </li>
+        <li>
+          <strong>Privacy Policy URL</strong> - The URL that points the site&apos;s privacy policy.
+        </li>
+      </ul>
+      <HelpHeading variant="h3">Menu & Logo</HelpHeading>
+      <p>
+        This section controls the top navigation menu (desktop) and the pull out navigation drawer (tablet and mobile),
+        as well as the Logo displayed in both. It also controls the text for the dontation button.
+      </p>
+      <p>This section includes:</p>
+      <strong>Logo</strong>
+      <ul>
+        <li>
+          <strong>Primary</strong> - The top text on the logo. The first letter of each word is rendered in a larger
+          font.
+        </li>
+        <li>
+          <strong>Secondary</strong> - The bottom text on the logo. The first letter of each word is rendered in a
+          larger font.
+        </li>
+      </ul>
+      <strong>Online Giving Button Text</strong>
+      <ul>
+        <li>
+          <strong>Online Giving Button Text</strong> - The text for the dontation button.
+        </li>
+      </ul>
+      <strong>Menu Items</strong>
+      <p>
+        This section controls the top navigation menu (desktop) and the pull out navigation drawer (tablet and mobile).
+        You can reorder them by dragging the items. Children CANNOT be dragged to the top level though.
+      </p>
+      <ul>
+        <li>
+          <strong>Title</strong> - The text displayed for the menu item
+        </li>
+        <li>
+          <strong>URL</strong> - Optional. The url to send the user to when they click/tap the link. Leave blank and
+          nothing happens on click/tap. Takes priority over page.
+        </li>
+        <li>
+          <strong>Page</strong> - Optional. The page to send the user to when they click/tap the link. Leave blank and
+          nothing happens on click/tap.
+        </li>
+        <li>
+          <strong>Menu Items</strong> - A list of sub menu items to display in a sub menu when the user hovers, clicks
+          or taps the top level menu item.
+          <ul>
+            <li>
+              <strong>Title</strong> - The text displayed for the menu item
+            </li>
+            <li>
+              <strong>URL</strong> - Optional. The url to send the user to when they click/tap the link. Leave blank and
+              nothing happens on click/tap. Takes priority over page.
+            </li>
+            <li>
+              <strong>Page</strong> - Optional. The page to send the user to when they click/tap the link. Leave blank
+              and nothing happens on click/tap.
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <HelpHeading variant="h3">Site Styles</HelpHeading>
+      <p>
+        This section controls styles throughout the site. This section is rather incomplete at the moment and I will
+        expand it more in the future.
+      </p>
+      <ul>
+        <li>
+          <strong>Footer Background</strong> - The image used in the background of the footer section of the site
+        </li>
+      </ul>
+      <HelpHeading variant="h2">Events (Google Calendar)</HelpHeading>
+      <p>This is an external link leading to Google Calendar</p>
+      <HelpHeading variant="h2">Forms (Google Drive)</HelpHeading>
+      <p>This is an external link leading to Google Drive</p>
     </StyledContent>
   );
 };
